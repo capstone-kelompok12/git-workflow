19a3b06ae3384f66d8c981355cec951b28aa08e4
anangkf_ <gonanggoneng@gmail.com>
2022-11-21 15:09:00 +0700

    added feature hello

--- main.js
+++ main.js
@@ -8,3 +8,9 @@ console.log('Hello World!')
 console.log('check branch Ancase')
 console.log('Ayo conflict')
 console.log('Awas ga conflict!')
+
+const hello = (name) =>{
+  return `Hi, ${name}!`
+}
+
+console.log(hello('Abghi'))
